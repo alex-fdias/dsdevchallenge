fddf3d09825992e1ea28c51ca10ecad807444c8d
amfdias <73295477+amfdias@users.noreply.github.com>
2022-01-14 17:30:39 +0000

    removed unnecessary lines and comments

--- main.py
+++ main.py
@@ -4,20 +4,12 @@ from imageprocessing import ImageProcessing
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-print_debug_info", action="store_true", help="print some debugging info")
     parser.add_argument("-save_debug_imgs" , action="store_true", help="save some debugging images")
-    opt = parser.parse_args()    
-    print_debug_info = opt.print_debug_info
-    save_debug_imgs  = opt.save_debug_imgs
+    opt = parser.parse_args()
     
-    img_processing = ImageProcessing(save_debug_imgs=False)
+    img_processing = ImageProcessing(save_debug_imgs=opt.save_debug_imgs)
     img_processing.run()
     img_processing.plot_distribution()
 
 if __name__ == '__main__':
     main()
-    
-    
-    
-    # no final: rever python meetup (PEP8)
-    # remove comments
